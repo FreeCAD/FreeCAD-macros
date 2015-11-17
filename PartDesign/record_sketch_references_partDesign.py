9f8025c950a86194d261b60738a7647864a9324f
hamish2014 <antoine.dymond@gmail.com>
2015-11-17 10:48:23 +0200

    progress bar added, and further patching for broken references

--- PartDesign/record_sketch_references_partDesign.py
+++ PartDesign/record_sketch_references_partDesign.py
@@ -7,8 +7,13 @@ reference_state = [] #list instead of dictionary to preserve order.
 for obj in FreeCAD.ActiveDocument.Objects:
     if obj.TypeId == 'Sketcher::SketchObject':
         if obj.ExternalGeometry != [] or obj.Support != None:
-            debugPrint(2, 'parsing ShapeElementReferences from %s' % obj.Name )
-            exGeom = [ ShapeElementReference(se_obj, se) for se_obj,se in obj.ExternalGeometry ]
+            debugPrint(3, 'parsing ShapeElementReferences from %s' % obj.Name )
+            exGeom = []
+            for se_obj,se in obj.ExternalGeometry:
+                try:
+                    exGeom.append( ShapeElementReference(se_obj, se) )
+                except RuntimeError, msg:
+                    FreeCAD.Console.PrintError('unable to record %s.%s (%s), reference will be removed on update!\n' % ( se_obj.Name, se, str(msg) ) )
             if obj.Support != None:
                 assert len( obj.Support[1] ) == 1
                 try:
@@ -18,6 +23,11 @@ for obj in FreeCAD.ActiveDocument.Objects:
             else:
                 supportGeom = None
             reference_state.append( [ obj.Name, exGeom, supportGeom ] )
-
+    elif obj.TypeId == 'PartDesign::LinearPattern':
+        refObj, refElements = obj.Direction
+        if not refObj.TypeId == 'Sketcher::SketchObject' and len(refElements) == 1:
+            debugPrint(3, 'parsing ShapeElementReferences from %s' % obj.Name )
+            supportGeom = ShapeElementReference( refObj, refElements[0])
+            reference_state.append( [ obj.Name, None, supportGeom ] )
 lib_repair_sketch_references_partDesign.reference_state = reference_state
 debugPrint( 1, 'Sketch references recorded', timePrefix=True )
--- PartDesign/repair_sketch_references_partDesign.py
+++ PartDesign/repair_sketch_references_partDesign.py
@@ -1,6 +1,7 @@
 
 import lib_repair_sketch_references_partDesign
 from lib_repair_sketch_references_partDesign import *
+from PySide import QtGui, QtCore
 
 
 debugPrint(2,'repair_sketch_references started',timePrefix=True)
@@ -8,32 +9,74 @@ debugPrint(2,'repair_sketch_references started',timePrefix=True)
 reference_state = lib_repair_sketch_references_partDesign.reference_state
 if len( reference_state ) == 0:
     raise ValueError, "record_sketch_references needs to be run first"
-for sketch_name, exGeom, supportGeom in reference_state:
-    debugPrint(3,'  checking %s' % sketch_name)
-    sketch = FreeCAD.ActiveDocument.getObject( sketch_name )
-    assert sketch != None
-    changed = False
 
-    newExtGeom = []
-    for g in exGeom:
-        se_name = g.getSubElementName()
-        old_name = g.description.subElementName
-        if se_name != old_name:
+FreeCAD.ActiveDocument.openTransaction('repair_references') #undo
+
+progress_n = len( reference_state )
+progress_ind = 0
+progressDialog = QtGui.QProgressDialog( "Repair Progress", "Cancel", 0, progress_n )
+progressDialog.setWindowModality(QtCore.Qt.WindowModal)
+progressDialog.forceShow()
+
+
+for obj_name, exGeom, supportGeom in reference_state:
+    if progressDialog.wasCanceled():
+        break
+    progressDialog.setLabelText(obj_name)
+    progressDialog.setValue( progress_ind )
+    progress_ind = progress_ind + 1
+
+    obj = FreeCAD.ActiveDocument.getObject( obj_name )
+    if obj.TypeId == 'Sketcher::SketchObject':
+        sketch_name = obj_name
+        sketch = obj
+        debugPrint(3,'  checking %s' % sketch_name)
+        sketch = FreeCAD.ActiveDocument.getObject( sketch_name )
+        assert sketch != None
+        changed = False
+
+        newExtGeom = []
+        for g in exGeom:
+            se_name = g.getShapeElementName()
+            old_name = g.description.shapeElementName
+            if se_name != old_name:
+                changed = True
+                debugPrint( 2,'  %s.ExternalGeometry:  %s -> %s' % (sketch_name, old_name, se_name) )
+            newExtGeom.append( ( g.object, se_name) )
+        if len( newExtGeom ) != len( sketch.ExternalGeometry ) : # then deletion of unparasable external geometry
+            debugPrint( 2,'  %s.ExternalGeometry trimmed to remove unparasable external geometry' % sketch_name )
             changed = True
-            debugPrint( 2,'  ExternalGeometry: %s -> %s' % (old_name, se_name) )
-        newExtGeom.append( ( g.object, se_name) )
 
-    if supportGeom != None:
-        se_name = supportGeom.getSubElementName()
-        old_name = supportGeom.description.subElementName
+        if supportGeom != None:
+            se_name = supportGeom.getShapeElementName()
+            old_name = supportGeom.description.shapeElementName
+            if se_name != old_name:
+                changed = True
+                debugPrint( 2,'  %s.Support:  %s -> %s' % (sketch_name, old_name, se_name) )
+                sketch.Support = ( supportGeom.object, [se_name] )
+
+        if changed:
+            sketch.ExternalGeometry = newExtGeom 
+            FreeCAD.ActiveDocument.recompute()
+            debugPrint(3,'  FreeCAD.ActiveDocument.recompute()')
+            #some sketch does not update correctly the first time ...
+            sketch.solve()
+            FreeCAD.ActiveDocument.recompute()
+
+
+    elif obj.TypeId == 'PartDesign::LinearPattern':
+        se_name = supportGeom.getShapeElementName()
+        old_name = supportGeom.description.shapeElementName
         if se_name != old_name:
-            changed = True
-            debugPrint( 2,'  supportGeom: %s->%s' % (old_name, se_name) )
-            sketch.Support = ( supportGeom.object, [se_name] )
+            debugPrint( 2,'  %s.Direction:  %s -> %s' % (obj_name, old_name, se_name) )
+            obj.Direction = ( supportGeom.object, [se_name] )
+            obj.touch()
+            FreeCAD.ActiveDocument.recompute()
 
-    if changed:
-        sketch.ExternalGeometry = newExtGeom
-        FreeCAD.ActiveDocument.recompute()
-        debugPrint(3,'  FreeCAD.ActiveDocument.recompute()')
+if not progressDialog.wasCanceled():
+    progressDialog.setValue( progress_ind )
+    debugPrint(1,'repair_sketch_references completed',timePrefix=True)
+else:
+    debugPrint(1,'repair_sketch_references aborted, press undo',timePrefix=True)
 
-debugPrint(1,'repair_sketch_references completed',timePrefix=True)
+FreeCAD.ActiveDocument.commitTransaction() #undo
